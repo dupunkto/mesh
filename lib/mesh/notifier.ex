f77b48a4ae4c0844984fbb42488c404c8f38ea8b
Robin <robindev2019@outlook.com>
2026-06-06 19:39:48 +0200

    Change webhook message for up

--- lib/mesh/notifier.ex
+++ lib/mesh/notifier.ex
@@ -15,7 +15,7 @@ defmodule Mesh.Notifier do
     end
   end
 
-  defp content(peer, :up), do: "🟩 `#{peer}` can be reached by `#{Mesh.me()}`"
+  defp content(peer, :up), do: "🟩 `#{peer}` can be reached again by `#{Mesh.me()}`"
   defp content(peer, :down), do: "🟥 `#{peer}` cannot be reached by `#{Mesh.me()}`"
   defp content(peer, :still_down), do: "🟥 `#{peer}` is still unreachable from `#{Mesh.me()}`"
 end
