e1fe76b41f2e37f7e981885451e7b95e9451ffa5
Gijs <gijstenberg@gmail.com>
2026-06-09 22:12:37 +0200

    Fix wrong comment

--- lib/mesh/monitor.ex
+++ lib/mesh/monitor.ex
@@ -10,7 +10,7 @@ defmodule Mesh.Monitor do
   @poll_interval :timer.seconds(5)
   @followup_interval :timer.minutes(30)
   @timeout :timer.seconds(5)
-  @threshold 4 # send down notification after 3 missed pings
+  @threshold 4 # send down notification after 4 missed pings
 
   def start_link(peer) do
     GenServer.start_link(__MODULE__, peer)
